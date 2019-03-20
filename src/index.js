5c741f1a3abc5bc3b40644560a647f1af69a2fd0
Yahor270 <270@tut.by>
2019-03-20 11:59:23 +0200

    done, all tests passed

--- src/index.js
+++ src/index.js
@@ -1,52 +1,56 @@
 module.exports = function solveSudoku(matrix) {
 
- var m=matrix;
- function rowCheck(row,num){
-   for(i=0;i<9;i++) if(m[row][i]==num) return false;
+ function rowCheck(m,row,num){
+   for(var i=0;i<9;i++) if(m[row][i]==num) return false;
    return true;
  }
 
-
-function colCheck(col,num){
-  for(i=0;i<9;i++) if(m[i][col]==num) return false;
+function colCheck(m,col,num){    
+  for(var i=0;i<9;i++) if(m[i][col]==num) return false;
   return true;
 }
 
-function boxCheck(row,col,num){
-  r=row-row%3;
-  c=col-col%3;
-  for(i=r;i<r+3;i++){
-    for(j=c;j<c+3;j++) if(m[i][j]==num) return false;
+function boxCheck(m,row,col,num){   //check 3x3 box
+  var r=row-row%3;
+  var c=col-col%3;
+  for(var i=r;i<r+3;i++){
+    for(var j=c;j<c+3;j++) if(m[i][j]==num) return false;
   }
   return true;
 }
 
-function numFits(row,col,num){
-  return rowCheck(row,num) && colCheck(col,num) && boxCheck(row,col,num);
+function numFits(m,row,col,num){
+  return rowCheck(m,row,num) && colCheck(m,col,num) && boxCheck(m,row,col,num);
 }
 
-function tryOut(){
-
-for(row=0;row<=8;row++){
-  for(col=0;col<=8;col++){
-    if(m[row][col]==0){
-      for(num=1;num<=9;num++){    
-        if(numFits(row,col,num)) {
-          //console.log('fit '+num);
-          m[row][col]=num;
-          if(tryOut()) return true;
-          m[row][col]=0; 
-        } 
-      }
-      
+function findHim(m){             //search for zero
+  for(var row=0;row<=8;row++){
+    for(var col=0;col<=8;col++){
+      if(m[row][col]==0) return [row, col];
     }
-    if(row==8 && col==8) return true;
-  }
-}
-return false;
+  }  
+  return [9,9];  //if not found
 }
 
-tryOut();
+function tryOut(m){
+var found=findHim(m);
+var row=found[0]; 
+var col=found[1]; 
+if(row==9) {
+  //solved
+  return true;
+} 
+      for(var num=1;num<=9;num++){    
+        if(numFits(m,row,col,num)) {
+          m[row][col]=num;
+          if(tryOut(m)) return true;
+          m[row][col]=0; 
+        }      
+      }
+    return false; 
+}
 
-return m;
+tryOut(matrix);
+
+return matrix;
 }
